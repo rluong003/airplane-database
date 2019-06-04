a5e71cc75b84d5ad8dd73bc0f4c661832b610e4f
Richard Luong <rluon003@bolt.cs.ucr.edu>
2019-06-03 20:52:29 -0700

    added index for ID's

---
+++ sql/createindex.sql
@@ -0,0 +1,12 @@
+/*
+indexes for faster queries
+
+
+*/
+CREATE INDEX id_index ON Pilot(id);
+CREATE INDEX id_index1 ON Flight(fnum);
+CREATE INDEX id_index2 ON Plane(id);
+CREATE INDEX id_index3 ON Technician(id);
+
+
+
